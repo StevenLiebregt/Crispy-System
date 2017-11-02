0335cea427d0f0e1dcef204a285af7e2bfdc6751
Steven Liebregt <s.liebregt@outlook.com>
2017-11-02 10:35:56 +0100

    fix

--- src/CrispySystem.php
+++ src/CrispySystem.php
@@ -30,7 +30,7 @@ class CrispySystem extends Container
             showPlainError('Storage directory is not writable, try running `chmod -R 0777 storage` in your root');
         }
 
-        if (!file_exist(ROOT . 'config' || !is_dir(ROOT . 'config')) {
+        if (!file_exists(ROOT . 'config') || !is_dir(ROOT . 'config')) {
             showPlainError('Config directory does not exist, please create one in your root');
         }
 
